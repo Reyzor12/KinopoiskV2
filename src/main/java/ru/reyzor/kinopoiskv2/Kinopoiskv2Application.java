569e7a11d24ded7dca0a4641ca82ca05f7648dec
reyzor <testaccwow001@gmail.com>
2018-09-12 08:54:09 +0300

    init repo

---
+++ src/main/java/ru/reyzor/kinopoiskv2/Kinopoiskv2Application.java
@@ -0,0 +1,12 @@
+package ru.reyzor.kinopoiskv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kinopoiskv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kinopoiskv2Application.class, args);
+	}
+}
---
+++ src/test/java/ru/reyzor/kinopoiskv2/Kinopoiskv2ApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.reyzor.kinopoiskv2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kinopoiskv2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
